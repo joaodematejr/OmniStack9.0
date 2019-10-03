fa174b36db32197a4b5d5c3e8eacbe2dbad54aa9
joaodematejr <joaodematejr@gmail.com>
2019-10-03 14:27:30 -0300

    Finalização do projeto web usando react e atualização do backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require('mongoose');
 const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 
@@ -14,8 +16,9 @@ mongoose.connect('mongodb+srv://joaodematejr:joaodematejr@cluster0-ardag.mongodb
 //req.query = ACESSAR QUERY PARAMS (PARA FILTRO)
 //req.params = ACESSAR ROUTE PARAMS (PARA EDITAR E DELETAR)
 //req.body = ACESSAR CORPO DA REQUISIÇÃO (PARA CRIAÇÃO, EDIÇÃO)
-
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
